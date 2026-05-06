67e7e72593ecadf5c1cf34324e0d544321f021ad
Jeanette Rusli <jeanrusli@gmail.com>
2026-05-06 20:59:25 +0700

    Add Connection Error class

---
+++ lib/errors.rb
@@ -0,0 +1,19 @@
+module Errors
+  class DynamicPricingError < StandardError
+    attr_reader :error_code
+
+    def initialize(message = nil, error_code: nil)
+      super(message)
+      @error_code = error_code
+    end
+  end
+
+  class ConnectionError < DynamicPricingError
+    def initialize(message = "Connection Error")
+      super(
+        message,
+        error_code: 500
+      )
+    end
+  end
+end
