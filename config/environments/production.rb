a9153ac29f48c7cecc58990171511ac2039a9879
Jeanette Rusli <jeanrusli@gmail.com>
2026-05-07 11:37:08 +0700

    Enable Rails Cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,7 @@ Rails.application.configure do
   # want to log everything, set the level to "debug".
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
+  # TODO: Enable cache.
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
